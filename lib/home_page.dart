007444667341687b42c33bfa913f508cfd6772e3
daringcomrade_Sarika <sarikapurohit790@gmail.com>
2023-10-21 18:58:27 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    final int days=30;
+    final String name= "Codepur";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of Flutter with $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
